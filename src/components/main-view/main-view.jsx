033230d72af1c1f5fcab950698e94a5e0f775429
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-04-30 16:47:10 +0200

    add useEffect hook to fetch data from API and instal ProtoTypesl ProtoTypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://testingmovieapi-l6tp.onrender.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((data) => {
+          return {
+            id: data._id,
+            title: data.title,
+            image: data.imageUrl,
+            description: data.description,
+            genre: data.genre, 
+            director: data.director.name,
+            featured: data.featured
+          };
+        });
+  
+        setMovies(moviesFromApi);
+      });
+  }, []);  
+  
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
