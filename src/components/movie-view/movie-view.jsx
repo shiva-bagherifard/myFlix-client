10bedd9223b0e5015dc115f97f1dd972e1057ff2
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-06-03 20:59:22 +0200

    updated

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export const MovieView = ({ movies }) => {
   const { movieId } = useParams();
+
   const movie = movies.find((m) => m.id === movieId);
 
+  // Handle case when movie is not found
   if (!movie) {
     return <div>Movie not found</div>;
   }
@@ -13,11 +16,11 @@ export const MovieView = ({ movies }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} className="img-fluid justify-content-md-center" alt="Movie Poster" />
+        <img src={movie.imageUrl} className="img-fluid  justify-content-md-center" alt="Movie Poster" />
       </div>
       <div>
-        <span style={{ fontWeight: 'bold' }}>Title: </span>
-        <span>{movie.title}</span>
+        <span style={{fontWeight: 'bold' }}>Title: </span>
+        <span style={{fontWeight: 'bold' }}>{movie.title}</span>
       </div>
       <div>
         <span>Description: </span>
@@ -28,14 +31,15 @@ export const MovieView = ({ movies }) => {
         <span>{movie.genre}</span>
       </div>
       <div>
+        {/* Check if director exists before accessing its properties */}
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.director ? movie.director.name : "Unknown"}</span>
       </div>
       <div>
         <span>Featured: </span>
         <span>{movie.featured ? "True" : "False"}</span>
       </div>
-      <Link to="/">
+      <Link to={`/`}>
         <button className="back-button">Back</button>
       </Link>
     </div>
@@ -48,8 +52,10 @@ MovieView.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     genre: PropTypes.string.isRequired,
-    director: PropTypes.string.isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }),
     featured: PropTypes.bool.isRequired,
-    image: PropTypes.string.isRequired,
-  })).isRequired,
+    imageUrl: PropTypes.string.isRequired,
+  })).isRequired
 };
