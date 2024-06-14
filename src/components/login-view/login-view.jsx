196c9f33d0c20e0ae01cc12136330346d45e39ee
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-06-14 17:08:30 +0200

    updated

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,17 +2,14 @@ import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Spinner from 'react-bootstrap/Spinner'; // Add spinner for loading state
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false); // Add loading state
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setLoading(true); // Set loading state to true
 
     const data = {
       username: username,
@@ -29,22 +26,20 @@ export const LoginView = ({ onLoggedIn }) => {
       });
 
       const responseData = await response.json();
-      console.log('Response Data:', responseData); // Log the response data
 
       if (response.ok) {
+        console.log(responseData)
         localStorage.setItem('user', JSON.stringify(responseData.user));
         localStorage.setItem('token', responseData.token);
         onLoggedIn(responseData.user, responseData.token); // Invoking onLoggedIn
         alert('Login successful');
-        setError(''); // Clear any previous error
       } else {
         throw new Error(responseData.error || 'Unknown error');
       }
     } catch (error) {
       console.error('Error during login:', error);
-      setError(error.message || 'Login failed. Please check your credentials.');
-    } finally {
-      setLoading(false); // Set loading state to false
+      setError('Login failed. Please check your credentials.');
+      alert('Login failed. Please check your credentials.');
     }
   };
 
@@ -73,8 +68,8 @@ export const LoginView = ({ onLoggedIn }) => {
             required
           />
         </Form.Group>
-        <Button variant="primary" type="submit" disabled={loading}>
-          {loading ? <Spinner animation="border" size="sm" /> : 'Submit'} // Show spinner if loading
+        <Button variant="primary" type="submit">
+          Submit
         </Button>
       </Form>
       {error && <div className="alert alert-danger mt-3">{error}</div>}
